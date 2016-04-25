a205a54ee4591b95da890b7bb2802107c8d910eb
yyx <yangyixiaofirst@163.com>
2016-04-25 23:19:53 +0800

    small change.

--- testresolve/src/main/java/cn/yyx/test/files/test.java
+++ testresolve/src/main/java/cn/yyx/test/files/test.java
@@ -1,14 +1,19 @@
 package cn.yyx.test.files;
 
-public class test {
+public class test extends Object{
+	
+	public test()
+	{
+		super();
+	}
 	
 	public String Mett()
 	{
 		// ASTParser parser = ASTParser.newParser(AST.JLS8);
 		// PPtest.PPtestHa();
 		// System.out.println(parser);
-		Class<?>[] ar = new Class<?>[]{A.class, B.class};
-		System.out.println(ar);
+		// Class<?>[] ar = new Class<?>[]{A.class, B.class};
+		// System.out.println(ar);
 		return "ha ewewo";
 	}
 	
